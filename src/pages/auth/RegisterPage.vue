f81b97bb6fa818829d350366e232557440208c35
BrettOrmsby <brettwormsby@outlook.com>
2023-03-10 12:41:50 -0500

    Feat: Redesign register page

--- src/pages/auth/RegisterPage.vue
+++ src/pages/auth/RegisterPage.vue
@@ -5,20 +5,35 @@ export default {
 </script>
 
 <template>
-  <form @submit.prevent="handleSubmit">
+  <div class="container">
     <h1>Register</h1>
-    <label>Username <input v-model="form.username" type="text" /></label>
-    <label>Email <input v-model="form.email" type="email" /></label>
-    <label>Password <input v-model="form.password" type="password" /></label>
-    <button>Register</button>
-  </form>
+    <form @submit.prevent="handleSubmit()">
+      <label for="username">Username</label>
+      <InputText id="username" v-model="form.username" maxlength="30" />
+
+      <label for="email">Email</label>
+      <InputText id="email" v-model="form.email" type="email" />
+      <label for="password">Password</label>
+      <Password
+        id="password"
+        v-model="form.password"
+        :feedback="false"
+        toggleMask
+      />
+
+      <Button label="Register" type="submit" :loading="loading" />
+    </form>
+  </div>
 </template>
 
 <script lang="ts" setup>
-//TODO: name cannot be me
 import { ref } from "vue";
 import useAuthUser from "@/composables/UseAuthUser";
 import { useRouter } from "vue-router";
+import throwError from "@/lib/thowError";
+import InputText from "primevue/inputtext";
+import Password from "primevue/password";
+import Button from "primevue/button";
 
 const router = useRouter();
 const { register } = useAuthUser();
@@ -29,7 +44,10 @@ const form = ref({
   password: "",
 });
 
+const loading = ref(false);
+
 const handleSubmit = async () => {
+  loading.value = true;
   try {
     await register(form.value);
 
@@ -38,9 +56,38 @@ const handleSubmit = async () => {
       query: { email: form.value.email },
     });
   } catch (error: any) {
-    alert(error.message);
+    if (
+      error.message ===
+      'duplicate key value violates unique constraint "profiles_username_key"'
+    ) {
+      throwError({
+        message: "Username Not Unique",
+        hint: "Username must be unique ",
+        details: "",
+        code: "",
+      });
+    } else {
+      throwError(error);
+    }
   }
+  loading.value = false;
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  padding: var(--content-padding);
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+label {
+  display: block;
+  margin-bottom: var(--inline-spacing);
+}
+:deep() :is(.p-inputtext, .p-password) {
+  display: block;
+  margin-bottom: var(--inline-block-spacing);
+  width: 100%;
+}
+</style>
