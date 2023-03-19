0897673212eb95d3e94d124cbc6719a7f0961743
BrettOrmsby <brettwormsby@outlook.com>
2023-03-19 09:38:59 -0400

    Build: attempt to fix bug for web history

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import useSupabase from "@/composables/UseSupabase";
 import throwError from "@/lib/thowError";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       name: "EmailConfirmation",
