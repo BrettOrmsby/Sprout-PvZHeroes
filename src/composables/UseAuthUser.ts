b6f3b7ab78f7c7e74777b30896bae0df17029a93
BrettOrmsby <brettwormsby@outlook.com>
2024-12-28 20:13:10 -0500

    refractor: remove useless email and pasword auth

--- src/composables/UseAuthUser.ts
+++ src/composables/UseAuthUser.ts
@@ -5,10 +5,9 @@ import { computed, ref } from 'vue'
 // user is set outside of the useAuthUser function
 // so that it will act as global state and always refer to a single user
 const user = ref<null | User>(null)
+const { supabase } = useSupabase()
 
 export default function useAuthUser() {
-  const { supabase } = useSupabase()
-
   const signIn = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'discord',
@@ -27,55 +26,11 @@ export default function useAuthUser() {
   const isSignedIn = computed(() => !!user.value)
   const id = computed(() => user.value?.id)
 
-  const register = async (credentials: { email: string; password: string; username: string }) => {
-    const { data, error } = await supabase.auth.signUp({
-      email: credentials.email,
-      password: credentials.password,
-      options: {
-        data: {
-          username: credentials.username,
-        },
-      },
-    })
-    if (error) {
-      throw error
-    } else {
-      if (data?.user?.identities?.length === 0) {
-        throw new Error('User with email already exists')
-      }
-    }
-    return data.user
-  }
-
-  /**
-   * Update user email or password
-   */
-  const update = async (updates: {
-    password?: string
-    email?: string
-    // eslint-disable-next-line
-    data?: Record<string, any>
-  }) => {
-    const { data, error } = await supabase.auth.updateUser(updates)
-    if (error) throw error
-    return data.user
-  }
-
-  const sendPasswordRestEmail = async (email: string) => {
-    const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: `${window.location.origin}/password-reset?fromEmail=registrationConfirmation`,
-    })
-    if (error) throw error
-  }
-
   return {
     user,
     id,
     signIn,
     isSignedIn,
     signOut,
-    register,
-    update,
-    sendPasswordRestEmail,
   }
 }
